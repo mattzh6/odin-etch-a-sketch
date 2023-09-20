01be9c17f4de0df2c0cdb21151947a7c63493590
Matthew Zhou <phoisgood1@gmail.com>
2023-09-20 11:27:36 -0700

    changed the grid to have a fixed width

--- script.js
+++ script.js
@@ -1,21 +1,48 @@
+const space = 960;
 const divContainer = document.querySelector(".container");
+const resizeButton = document.querySelector("button.resize");
 
-for (let i = 0; i < 16; i++) {
-    const div = document.createElement("div");
-    for (let j = 0; j < 16; j++) {
-        const square = document.createElement("div");
-        square.classList.add("square")
-        div.appendChild(square);
+
+function resizePrompt(event) {
+    let dimension = -1;
+    while (dimension <= 0) {
+        let userChoice = prompt("Please enter your desired grid dimension");
+        let parsedChoice = parseInt(userChoice);
+        if (parsedChoice && parsedChoice <= 100) {
+            dimension = parsedChoice;
+        }
     }
-    div.classList.add("rows");
-    divContainer.appendChild(div);
+
+    divContainer.replaceChildren();
+    resizeGrid(dimension);
 }
+
+function resizeGrid(dimension) {
+    for (let i = 0; i < dimension; i++) {
+        const div = document.createElement("div");
+        for (let j = 0; j < dimension; j++) {
+            const square = document.createElement("div");
+            square.classList.add("square")
+            square.style.width = 960/dimension + "px";
+            square.style.height = 960/dimension + "px";
+            div.appendChild(square);
+        }
+        divContainer.appendChild(div);
+    }
+
+    const squares = document.querySelectorAll(".square");
+    squares.forEach((square) => {
+        square.addEventListener("mouseover", changeColor);
+    });
+}
+
+resizeGrid(16);
 
 function changeColor(e) {
     this.classList.add("drawn");
 }
 
-const squares = document.querySelectorAll(".square");
-squares.forEach((square) => {
-    square.addEventListener("mouseover", changeColor);
-});
+
+
+resizeButton.addEventListener("click", resizePrompt);
+
